0f508da2f53336c1ffbd12e1ac8d58ac84cdf0ff
Jose Antonio Alvino Velasque <jalvinov@everis.com>
2021-01-06 11:30:18 -0500

    Swagger implemented

--- src/main/java/com/bank/fingerprints/controller/FingerprintController.java
+++ src/main/java/com/bank/fingerprints/controller/FingerprintController.java
@@ -4,6 +4,10 @@ import com.bank.fingerprints.entity.Fingerprint;
 import com.bank.fingerprints.entity.FingerprintResponse;
 import com.bank.fingerprints.service.FingerPrintService;
 import io.reactivex.Single;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/core/fingerprints")
+@Api("/core/fingerprints")
 public class FingerprintController
 {
     @Autowired
     FingerPrintService fingerPrintService;
 
     @PostMapping("/validate")
+    @ApiOperation(value = "Validate if people are registered by their fingerprint", notes = "Retrieving a Fingerprint Response", response = FingerprintResponse.class)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = FingerprintResponse.class)
+    })
     public Single<FingerprintResponse> validateFingerPrintByDocument(@RequestBody Fingerprint fingerPrint)
     {
         return fingerPrintService.validateFingerPrint(fingerPrint.getDocument());
